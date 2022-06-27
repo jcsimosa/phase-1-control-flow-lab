7adfbe46e6cf750fa426fa0cd35037a64d934566
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-27 15:27:30 -0500

    commit tree 02e25b0873fd9a3292ee63fc3be9149c75ad6d44

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return 'This one is on me!'
+  }  
+   else if (ride > 400 && ride <=2000){
+    return 'That will be twenty bucks.'
+   }
+   else if (ride <= 2500){
+    return 'I will gladly take your thirty bucks.'
+   }
+   else {
+    return 'No can do.'
+   }
+  }
+function ternaryCheckCity(city){
+
+ const whatCity = city === 'NYC' ? ("Ok, sounds good.") : ("No go.") 
+  return whatCity
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let message;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default: 
+      return 'Bye.'
+      break;      
+  }
 }
